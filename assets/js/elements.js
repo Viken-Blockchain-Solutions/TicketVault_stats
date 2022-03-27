ce67ea01b1fa1fc28cc6f10019ff43186331e3fc
Ivo Garofalo <41997352+Dadogg80@users.noreply.github.com>
2022-03-28 00:44:47 +0200

    Spread

--- assets/js/elements.js
+++ assets/js/elements.js
@@ -1,41 +1,79 @@
-// Get access to DOM elements
-const choice = document.querySelector('choice');
+/* // Get access to DOM elements
 
 // radio buttons
-const asset = document.getElementById('radio-asset');
-const token = document.getElementById('radio-token');
+const gridRadios = document.querySelectorAll('gridRadios');
 
-const articleSection = document.querySelector('section');
+const asset_radio = document.getElementById('radio-asset');
+const token_radio = document.getElementById('radio-token');
 
-// add event listeners
-asset.addEventListener('click', () => {
-    console.log("working");
-});
+const radio_choice = document.getElementById("radio-choice");
 
-token.addEventListener('click', () => {
-    const choice = startSpreading();
-    console.log(choice);
-    articleSection.appendChild(choice);
-});
+asset_radio.addEventListener('change', (e) => {
+    if (e.target.checked) {
+        let asset = spreadAsset();
+        radio_choice.classList.add(asset)
+    } else {
+        let asset = spreadAsset();
+        radio_choice.classList.remove(asset)
+        
+    }
+})
+
+token_radio.addEventListener('change', (target) => {
+    // Append to another element:
+    document.getElementById("radio-choice").appendChild(spreadTokens()); 
+})
 
 
-const addOptions = `
+function spreadTokens() {
+    // create element:
+    const div = document.createElement("h4");
+    div.innerHTML = "Spread Tokens";
+
+    // Append to another element:
+    document.getElementById("radio-choice").class.remove(div);
+}
+
+function spreadAsset() {
+    const div = document.createElement("h4");
+    div.innerHTML = "Spread Asset";
+
+    // Append to another element:
+    document.getElementById("radio-choice").appendChild(div); 
+
+}
+
+
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const addOptions = (`
 <option selected>Choose a token to spread</option>
 <option value="0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619">ETH</option>
 <option value="0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6">BTC</option>
 <option value="0x7b9c2d37fc0B45A5Ec022D2023F18685FdA832e7">CENT</option>
 <option value="0xB25e20De2F2eBb4CfFD4D16a55C7B395e8a94762">REQ</option>
 <option value="0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270">WMATIC</option>
-`;
+`);
 
 
-// page functions.
-function startSpreading() {
-    let newArticle = document.createElement('label');
-    let newOption = document.createElement('option', "Choose a token to spread");
-    
-    newArticle.appendChild(newOption);
-    
-    newArticle.classList.add('section');
-    return newArticle;
-  }
+
