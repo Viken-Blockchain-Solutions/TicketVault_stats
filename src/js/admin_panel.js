df540cfbefd2034ec74d77ed5a69d5c29c0d9108
ivo garofalo <41997352+Dadogg80@users.noreply.github.com>
2022-04-12 01:48:19 +0200

    admin page

--- src/js/admin_panel.js
+++ src/js/admin_panel.js
@@ -1,38 +1,66 @@
+var output_listing = document.getElementById("output-listing");
 
-function auction() {
-    timed_auction.innerHTML = `
-    <div class="row mb-3">
-        <label for="inputTime" class="col-sm-2 col-form-label">Timed Auction</label>
+
+
+
+const timed_auction = `
+    <div class="row mb-4">
+        <label for="inputTime" class="col-sm-4 col-form-label">Timed Auction</label>
         <div class="col-sm-4">
         <input type="time" class="form-control">
         </div>
     </div>
     <div class="row mb-3">
-        <label for="inputDate" class="col-sm-2 col-form-label">Set start date</label>
-        <div class="col-sm-4">
+        <label for="inputDate" class="col-sm-4 col-form-label">Set start date</label>
+        <div class="col-sm-5">
         <input type="date" class="form-control">
         </div>
     </div>
 `;
-} 
 
-function fixed() {
-    timed_auction.innerHTML = `
-    <div class="row mb-3">
-        <label for="inputPrice" class="col-sm-2 col-form-label">Set Price</label>
+const fixed_price = `
+    <div class="row mb-4">
+        <label for="inputPrice" class="col-sm-3 col-form-label">Set Price</label>
         <div class="col-sm-4">
         <input type="text" class="form-control">
         </div>
     </div>
-    `;
+`;
+
+// display fixed pricing element
+function showFixed() {
+    output_listing.style.display = "block";
+    output_listing.innerHTML = fixed_price;
 }
 
+// display timed auction element
+function showTimed() {
+    output_listing.style.display = "block";
+    output_listing.innerHTML = timed_auction;
+}
+// display no listing options
+function showNone() {
+    output_listing.style.display = "none";
+}
 
+function json() {
+    var input_artist = document.getElementById("input-artist").value;
+    var input_collection = document.getElementById("input-collection").value;
+    var input_external_url = document.getElementById("input-external-url").value;
+    var input_genre = document.getElementById("input-genre").value;
+    var input_asset = document.getElementById("input-asset").value;
+    var input_media = document.getElementById("input-media").value;
+    var input_total_supply = document.getElementById("input-total-supply").value;
+    var input_description = document.getElementById("input-description").value;
+    
+    console.log("artist", input_artist);
 
-
-const gridRadios1 = document.querySelector("#gridRadios1").value;
-const gridRadios2 = document.querySelector("#gridRadios2").value;
-
-
-const timed_auction = document.getElementById("timedAuction");
-
+    document.getElementById("output-artist").innerHTML = `"${input_artist}",`;
+    document.getElementById("output-collection").innerHTML = `"${input_collection}",`;
+    document.getElementById("output-external-url").innerHTML = `"${input_external_url}"`;
+    document.getElementById("output-genre").innerHTML = `"${input_genre}",`;
+    document.getElementById("output-asset").innerHTML = `"${input_asset}",`;
+    document.getElementById("output-media").innerHTML = `"${input_media}",`;
+    document.getElementById("output-description").innerHTML = `"${input_description}",`;
+    document.getElementById("output-total-supply").innerHTML = `"${input_total_supply}",`;    
+}
