d99d93d3d903b25ffbf3f29b45cf8b7de87a1946
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2022-08-07 15:50:31 +0200

    Update

--- assets/js/spread/app.js
+++ assets/js/spread/app.js
@@ -1,74 +1,88 @@
-
+// checks the current user
 const checkUser = () => {
   let user = Moralis.User.current();
-  
-  if(user){
-    console.log("Logged in FLOW")
-    console.log("User Account :",user.get("ethAddress"))
-    console.log("about to set user address, IF USER WAS LOGGED IN AT ARRIVAL")
+
+  if(user) {
     let account = user.get("ethAddress");
     setCurrentUser(account);
-    console.log("user address should be displayed now")
   }
   else {
-    console.log("NOT logged in FLOW")
-    login(user)
+    // removeDisconnected();
+    login();
+    setCurrentUser(account);
   }
 }
-
-fetchAssets();
-
-
 
 /* Authentication code */
 async function login() {
   await Moralis.authenticate({
     signingMessage: "Welcome to Spread Dapp. Connect to spread assets!",
   })
-    .then(async = (user) => {
-      console.log("inside then.")
-      console.log("about to set user address, IF USER WAS NOT LOGGED IN AT ARRIVAL")
-      let account = user.get("ethAddress");
-      console.log("User Account :", account);
-      setCurrentUser(account);
-      console.log("user address should be displayed now")
-    })
+  .then(async = (user) => {
+    let account = user.get("ethAddress");
+    console.log("logged in account:", account);
+  })
 }
 
 async function disconnect() {
   await Moralis.User.logOut();
-  console.log("disconnected!");
+  removeDisconnected();
 }
 
+// Set current logged in user
 async function setCurrentUser(account) {
-  console.log("setting current user")
   let userAddress = document.getElementById("userAddress");
-
+  
+  fetchAssets();
   userAddress.style.display = "block";
   userAddress.innerHTML = account;
+
+  // listens to changed account in metamask
+  Moralis.onAccountChanged((changedAccount) => {
+    console.log(changedAccount);
+    
+    login();
+
+    console.log("logged in user", changedAccount);
+    console.log("about to fetch assets");
+    fetchAssets();
+    userAddress.innerHTML = changedAccount;
+  });
   
   document.getElementById("btn-login").style.display = "none";
   document.getElementById("btn-logout").style.display = "block";
 }
 
+// Remove user that has disconnected
+function removeDisconnected() {
+  let userAddress = document.getElementById("userAddress");
+  
+  userAddress.style.display = "none";
+  
+  document.getElementById("btn-login").style.display = "block";
+  document.getElementById("btn-logout").style.display = "none";
+}
+
+
 // get native assets and token balances.
 async function fetchAssets() {
   const web3Provider = await Moralis.enableWeb3();
   const network = await web3Provider.getNetwork();
-
+  
   let response = await fetch("https://chainid.network/chains.json");
   let data = await response.json();
-
+  
+  console.log(data);
   await getNativeAsset(network, data);
   await getERC20Assets();
-
+  
   selectAssets.addEventListener("change", (event) => token = event.target.value);
 }
 
 async function spread() {
   const web3Provider = await Moralis.enableWeb3();
   const network = await web3Provider.getNetwork();
-
+  
   if(selectAssets.value === "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee") sendNativeAsset(network);
   else if(selectAssets.value ===  "") {
     alert("Please select an asset to spread");
@@ -79,8 +93,6 @@ async function spread() {
 // is the user logged in or not
 // if user is logged in -> display address.
 // if the user is not logged in -> display login button.
-
-
 
 document.getElementById("btn-login").onclick = login;
 document.getElementById("btn-logout").onclick = disconnect;
